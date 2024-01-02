b85996f2c5020ccb1d2f59ef29cee7055543a10d
consultantvishalsingh <153588963+consultantvishalsingh@users.noreply.github.com>
2024-01-01 22:18:11 -0500

    Update C1_Tipcalculator.py

--- C1_Tipcalculator.py
+++ C1_Tipcalculator.py
@@ -1,3 +1,24 @@
+""" 
+# How To Read Input String using Split
+txt = "hello, my name is Peter, I am 26 years old"
+x = txt.split()# white space by default
+y = txt.split(" ")
+z = txt.split(",")
+a = txt.split("is")
+
+print(x)
+print(y)
+print(z)
+print(a)
+
+# Converting to Int (for(i=0;i<len(sud_ht);i++)   
+student_heights = input().split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+  
+"""
+
+
 print("Welcome to Tip Calculator")
 # 1st input:
 bill =float(input("Please Enter your bill $\n"))
