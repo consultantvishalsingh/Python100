d2918c20585bbcb51fe4f5b0e8f00488cc816231
consultantvishalsingh <153588963+consultantvishalsingh@users.noreply.github.com>
2024-01-03 18:00:29 -0500

    Update C4_RobotHurdle.py

--- C4_RobotHurdle.py
+++ C4_RobotHurdle.py
@@ -25,3 +25,19 @@ while not at_goal():
         move()
     else:
         jump()
+
+
+# HURDLE NUMBER 5 MAZE PROBLEM
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
